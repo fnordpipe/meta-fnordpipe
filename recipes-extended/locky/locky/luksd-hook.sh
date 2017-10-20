3b71816fabe7040d42dd8458f3b3dd8cc2e5c644
crito <crito@fnordpipe.org>
2017-10-20 23:56:55 +0200

    locky: fix aufs mountpoints in luksd-hook

--- recipes-extended/locky/locky/luksd-hook.sh
+++ recipes-extended/locky/locky/luksd-hook.sh
@@ -16,18 +16,18 @@ systemctl stop systemd-networkd.service
 systemctl stop systemd-resolved.service
 umount /etc/systemd/network
 mount -t aufs \
-  -o br=/usr/local/system/systemd/network:/etc/systemd/network \
-  /etc/systemd/network
+  none /etc/systemd/network \
+  -o br=/usr/local/system/systemd/network:/etc/systemd/network
 mount -t aufs \
-  -o br=/usr/local/system/root:/root \
-  /root
+  none /root \
+  -o br=/usr/local/system/root:/root
 mount -t aufs \
-  -o br=/usr/local/system/dropbear:/etc/dropbear \
-  /etc/dropbear
+  none /etc/dropbear \
+  -o br=/usr/local/system/dropbear:/etc/dropbear
 mount -t aufs \
-  -o br=/usr/local/system/etc/xen:/etc/xen \
-  /etc/xen
+  none /etc/xen \
+  -o br=/usr/local/system/xen:/etc/xen
 systemctl start systemd-networkd.service
 systemctl start systemd-resolved.service
-systemctl start dropbear.service
+systemctl start dropbear.socket
 systemctl start xendomains.service
